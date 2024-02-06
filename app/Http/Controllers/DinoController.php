aed5be08503daa4d03fde22f986eb2eb9688a7e3
Kantemir Satibalov <kantik001@yandex.ru>
2024-02-06 17:11:10 +0300

    create models, migrations,controllers

---
+++ app/Http/Controllers/DinoController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Dino;
+use Illuminate\Http\Request;
+
+class DinoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Dino $dino)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Dino $dino)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Dino $dino)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Dino $dino)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_02_06_130855_create_promocodes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('promocodes', function (Blueprint $table) {
+            $table->id();
+            $table->text('code');
+            $table->decimal('value');
+            $table->date('expired_at');
+            $table->integer('limit_from');
+            $table->string('user');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('promocodes');
+    }
+};
