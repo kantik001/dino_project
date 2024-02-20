cf24dc5fcdb946e1c10f55e60a1c07bacdefe1dd
Kantemir Satibalov <kantik001@yandex.ru>
2024-02-20 16:17:31 +0300

    add middleware

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Admin\OrderController;
 use App\Http\Controllers\Admin\ProfileController;
 use App\Http\Controllers\Admin\PromocodeController;
 use App\Http\Controllers\Admin\TransactionController;
+use App\Http\Controllers\Admin\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,10 +25,17 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('/dinos', DinoController::class);
-Route::apiResource('/buildings', BuildingController::class);
-Route::apiResource('/orders', OrderController::class);
-Route::apiResource('/profiles', ProfileController::class);
-Route::apiResource('/promocodes', PromocodeController::class);
-Route::apiResource('/transactions', TransactionController::class)->except('destroy');
+Route::group(['middleware' => 'auth.admin'], function ()
+{
+    Route::apiResource('/dinos', DinoController::class);
+    Route::apiResource('/buildings', BuildingController::class);
+    Route::apiResource('/orders', OrderController::class);
+    Route::apiResource('/profiles', ProfileController::class);
+    Route::apiResource('/promocodes', PromocodeController::class);
+    Route::apiResource('/transactions', TransactionController::class)->except('destroy');
+    Route::apiResource('/users', UserController::class);
+
+});
+
+
 
